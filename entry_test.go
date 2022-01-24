ec8de1b2316d79bc3575f23bbc9b70b301348dd2
diosurandito <diosurandito@gmail.com>
2022-01-24 16:16:24 +0700

    10.Entry

---
+++ entry_test.go
@@ -0,0 +1,17 @@
+package golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestEntry(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	entry := logrus.NewEntry(logger)
+	entry.WithField("username", "dio")
+	entry.Info("Hello entry")
+
+}
