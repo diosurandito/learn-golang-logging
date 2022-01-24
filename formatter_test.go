f83b33c2909812428838327247c110c865dc425f
diosurandito <diosurandito@gmail.com>
2022-01-24 15:25:28 +0700

    08.Formatter

---
+++ formatter_test.go
@@ -0,0 +1,16 @@
+package golang_logging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestFormatter(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.Info("This is Info")
+	logger.Warn("This is Warn")
+	logger.Error("This is Error")
+}
